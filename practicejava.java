75aa1605dbcd207be36c72d82fea8869ced6bcc1
prince7111 <princemeena7111@gmail.com>
2025-07-05 10:47:24 +0530

    practice java for push code

--- practicejava.java
+++ practicejava.java
@@ -1,5 +1,8 @@
-public class practicejava{
+package practice;
+
+public class practicejava {
     public static void main(String args[]){
-        System.out.println("5th july 2025 // semester two");
+        System.out.println("5th july 2025 //semester two");
     }
+    
 }
