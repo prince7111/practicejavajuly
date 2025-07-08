f106295186f3934e6c3e83d7e92358fa74a267f6
prince7111 <princemeena7111@gmail.com>
2025-07-08 21:54:35 +0530

    Update practicejava.java

--- practicejava.java
+++ practicejava.java
@@ -3,6 +3,7 @@ package practice;
 public class practicejava {
     public static void main(String args[]){
         System.out.println("5th july 2025 //semester two");
+        System.out.println("8th july 2025");
     }
     
 }
